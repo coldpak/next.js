79ef3edbe105fd74597f789f341f282497c0b7bc
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-05-28 21:14:04 +0200

    Turbopack: improve error message when tracing too much

--- crates/next-api/src/nft.rs
+++ crates/next-api/src/nft.rs
@@ -4,11 +4,12 @@ use std::{
 };
 
 use anyhow::{Context, Result, bail};
+use async_trait::async_trait;
 use bincode::{Decode, Encode};
 use next_core::{app_structure::FileSystemPathVec, next_config::NextConfig};
 use rustc_hash::{FxHashMap, FxHashSet};
 use tracing::Instrument;
-use turbo_rcstr::RcStr;
+use turbo_rcstr::{RcStr, rcstr};
 use turbo_tasks::{
     FxIndexMap, FxIndexSet, NonLocalValue, ReadRef, ResolvedVc, TryFlatJoinIterExt, TryJoinIterExt,
     Vc, trace::TraceRawVcs,
@@ -21,9 +22,12 @@ use turbo_tasks_hash::HashAlgorithm;
 use turbopack_core::{
     asset::Asset,
     chunk::{ChunkingType, TracedMode},
+    file_source::FileSource,
     ident::AssetIdent,
+    issue::{Issue, IssueExt, IssueSeverity, IssueSource, IssueStage, StyledString},
     module::{Module, Modules},
     module_graph::{GraphTraversalAction, ModuleGraph},
+    raw_module::RawModule,
 };
 
 use crate::project::Project;
@@ -78,6 +82,7 @@ pub async fn trace_endpoint(
                 .await?
                 .map(|v| *v),
             false,
+            Some(next_config.config_file_path(project_path.clone())),
         )
         .await?;
 
@@ -327,6 +332,7 @@ pub async fn traced_modules_for_entries(
     entry_modules: Vc<Modules>,
     exclude_glob: Option<Vc<Glob>>,
     entries_are_traced: bool,
+    forbidden_path: Option<Vc<FileSystemPath>>,
 ) -> Result<Vc<Modules>> {
     let exclude_glob = if let Some(exclude_glob) = exclude_glob {
         Some(exclude_glob.await?)
@@ -340,6 +346,18 @@ pub async fn traced_modules_for_entries(
         None
     };
 
+    let forbidden_module = if let Some(forbidden_path) = forbidden_path {
+        Some(ResolvedVc::upcast(
+            RawModule::new(Vc::upcast(FileSource::new(forbidden_path.owned().await?)))
+                .to_resolved()
+                .await?,
+        ))
+    } else {
+        None
+    };
+
+    let mut forbidden_issues = vec![];
+
     let mut traced_modules = FxIndexSet::default();
     module_graph.await?.traverse_edges_dfs(
         entry_modules.await?.iter().copied(),
@@ -351,6 +369,10 @@ pub async fn traced_modules_for_entries(
                 }
                 return Ok(GraphTraversalAction::Continue);
             };
+
+            if forbidden_module.is_some_and(|m| m == target) {
+                forbidden_issues.push((parent, ref_data.reference));
+            }
 
             if should_visit_for_tracing(&ref_data.chunking_type, traced_modules.contains(&parent)) {
                 if let Some(exclude_glob) = &exclude_glob
@@ -373,6 +395,16 @@ pub async fn traced_modules_for_entries(
         |_, _, _| Ok(()),
         true,
     )?;
+
+    for (parent, reference) in forbidden_issues {
+        ForbiddenTracedFileIssue::new(
+            parent.ident().await?.path.clone(),
+            reference.into_trait_ref().await?.source(),
+        )
+        .to_resolved()
+        .await?
+        .emit();
+    }
 
     Ok(Vc::cell(traced_modules.into_iter().collect()))
 }
@@ -462,6 +494,96 @@ pub async fn traced_module_data_for_graph(
         hashes: ResolvedVc::cell(hashes),
     }
     .cell())
+}
+
+#[turbo_tasks::value(shared)]
+struct ForbiddenTracedFileIssue {
+    parent: FileSystemPath,
+    issue_source: Option<IssueSource>,
+}
+
+#[turbo_tasks::value_impl]
+impl ForbiddenTracedFileIssue {
+    #[turbo_tasks::function]
+    pub async fn new(
+        parent: FileSystemPath,
+        issue_source: Option<IssueSource>,
+    ) -> Result<Vc<Self>> {
+        Ok(Self {
+            parent,
+            issue_source,
+        }
+        .cell())
+    }
+}
+
+#[async_trait]
+#[turbo_tasks::value_impl]
+impl Issue for ForbiddenTracedFileIssue {
+    fn severity(&self) -> IssueSeverity {
+        // Ideally this would be an error, but for now we keep it a warning to avoid breaking
+        // existing apps
+        IssueSeverity::Warning
+    }
+
+    fn stage(&self) -> IssueStage {
+        IssueStage::Misc
+    }
+
+    fn source(&self) -> Option<IssueSource> {
+        self.issue_source
+    }
+
+    async fn file_path(&self) -> Result<FileSystemPath> {
+        Ok(self.parent.clone())
+    }
+
+    async fn title(&self) -> Result<StyledString> {
+        Ok(StyledString::Text(rcstr!(
+            "Encountered unexpected file in NFT list"
+        )))
+    }
+
+    async fn description(&self) -> Result<Option<StyledString>> {
+        let stack = vec![
+            StyledString::Text(rcstr!(
+                "This import traced the next.config.js file which indicates that the whole \
+                 project was traced unintentionally. Somewhere in the import trace below, there \
+                 are:"
+            )),
+            StyledString::Line(vec![
+                StyledString::Text(rcstr!("- filesystem operations (like ")),
+                StyledString::Code(rcstr!("path.join")),
+                StyledString::Text(rcstr!(", ")),
+                StyledString::Code(rcstr!("path.resolve")),
+                StyledString::Text(rcstr!(" or ")),
+                StyledString::Code(rcstr!("fs.readFile")),
+                StyledString::Text(rcstr!("), or")),
+            ]),
+            StyledString::Line(vec![
+                StyledString::Text(rcstr!("- very dynamic requires (like ")),
+                StyledString::Code(rcstr!("require('./' + foo)")),
+                StyledString::Text(rcstr!(").")),
+            ]),
+            StyledString::Text(rcstr!("To resolve this, you can")),
+            StyledString::Text(rcstr!("- remove them if possible, or")),
+            StyledString::Text(rcstr!("- only use them in development, or")),
+            StyledString::Line(vec![
+                StyledString::Text(rcstr!(
+                    "- make sure they are statically scoped to some subfolder: "
+                )),
+                StyledString::Code(rcstr!("path.join(process.cwd(), 'data', bar)")),
+                StyledString::Text(rcstr!(", or")),
+            ]),
+            StyledString::Line(vec![
+                StyledString::Text(rcstr!("- add ignore comments: ")),
+                StyledString::Code(rcstr!(
+                    "path.join(/*turbopackIgnore: true*/ process.cwd(), bar)"
+                )),
+            ]),
+        ];
+        Ok(Some(StyledString::Stack(stack)))
+    }
 }
 
 /// The globs defined in the next.config.mjs are relative to the project root.
